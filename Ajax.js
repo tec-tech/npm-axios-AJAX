9d4118b739695d520829c82d0466fbcf0d6b691e
yoshihir0 <yoshi@tectech.jp>
2019-05-17 00:32:22 +0900

    test関数追加

--- Ajax.js
+++ Ajax.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const AJAX = {
+	test: function(){console.log('test from AJAX.');},	// test function
 	sender: null,	// apiにて「_self」でアクセス
 	$store: null,	// nuxtグローバルオブジェクト
 	sendCount : 0,
